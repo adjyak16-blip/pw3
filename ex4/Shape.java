885d4a10e87cfd03b0098613659b39b368f2f0a3
yakoub <adjyak16@gmail.com>
2026-05-06 10:53:40 +0100

    Update Shape.java

--- ex4/Shape.java
+++ ex4/Shape.java
@@ -1,8 +1,6 @@
 package ex4;
 
-// Abstract class cannot be instantiated
 public abstract class Shape {
-    // Abstract methods: no body here, implementation is in subclasses
     public abstract double area();
     public abstract double perimeter();
 }
