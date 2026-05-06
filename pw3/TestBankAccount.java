71ed30333f996af02e9431ec8c58e01885e0f2e1
yakoub <adjyak16@gmail.com>
2026-05-06 10:55:27 +0100

    Update TestBankAccount.java

--- pw3/TestBankAccount.java
+++ pw3/TestBankAccount.java
@@ -6,7 +6,6 @@ public class TestBankAccount {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Create an instance of BankAccount[cite: 7]
         System.out.print("Enter owner name: ");
         String name = sc.nextLine();
         
@@ -15,23 +14,19 @@ public class TestBankAccount {
 
         BankAccount account = new BankAccount(name, initialBalance);
 
-        // Perform deposit operation[cite: 7]
         System.out.print("\nEnter amount to deposit: ");
         double dep = sc.nextDouble();
         account.deposit(dep);
 
-        // Perform withdrawal operation[cite: 7]
         System.out.print("Enter amount to withdraw: ");
         double with = sc.nextDouble();
         account.withdraw(with);
 
-        // Observe behavior using setter[cite: 7]
-        sc.nextLine(); // Clear buffer
+        sc.nextLine(); 
         System.out.print("\nEnter a new name to update owner: ");
         String newName = sc.nextLine();
         account.setOwnerName(newName);
 
-        // Display final account information[cite: 7]
         System.out.println("\n--- Final Account Details ---");
         System.out.println(account.toString());
 
